44be04b1304255ebef1bd86c5b5c6a78934bb4b6
Edouard DA SILVA <esdasilva@hotmail.fr>
2023-09-09 09:35:55 +0200

    put basic style project issue #7

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,21 @@
 
 </script>
 <template>
-    <h1>Accueil</h1>
+    <header>
+        <h1>Portfolio</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Presentation</h2>
+        </section>
+        <section>
+            <h2>Projets</h2>
+        </section>
+        <section>
+            <h2>Contact</h2>
+        </section>
+    </main>
+    <footer>
+        copyright Edouard DA SILVA 2023
+    </footer>
 </template>
